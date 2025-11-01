91009227e8068e38b0293cb96f534577a986cbee
Md. Sahin Alan <sahinraj20@gmail.com>
2025-11-01 22:28:31 +0600

    added product controller

---
+++ backend/src/features/production/production.controller.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+import { addProduct, getProduct } from "./production.service";
+
+// add product
+export const addProductController = async (req: Request, res: Response) => {
+  try {
+    const { num } = req.body;
+    const result = await addProduct(Number(num));
+
+    res.status(200).json({
+      success: true,
+      message: "Product added successfully!",
+      data: result,
+    });
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Failed to add product",
+      error: err.message,
+    });
+  }
+};
+
+// get product
+export const getProductController = async (req: Request, res: Response) => {
+  try {
+    const result = await getProduct();
+
+    res.status(200).json({
+      success: true,
+      message: "Product fetched successfully!",
+      data: result,
+    });
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch product",
+      error: err.message,
+    });
+  }
+};
