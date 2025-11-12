188cabfd329c6f42a1329817640e1bff4f8db6a7
Md. Sahin Alan <sahinraj20@gmail.com>
2025-11-12 19:58:14 +0600

    created get attendance list service

--- backend/src/features/attendance/attendance.service.ts
+++ backend/src/features/attendance/attendance.service.ts
@@ -1,5 +1,13 @@
 import Attendance from "./attendance.model"
 
+
+// get attendences based on date
+export const getAttendance = async(date:string)=>{
+    const response = await Attendance.find({date});
+    return response;
+}
+
+
 // Update attendance
 export const updateAttendance = async(empId:string, status:'present'| 'absent'|'leave', date:string)=>{
     const response = await Attendance.findOneAndUpdate({employeeId:empId, date}, {status}, {new:true, upsert:true});
