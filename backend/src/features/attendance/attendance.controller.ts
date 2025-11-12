6b5c2c636a9da33443e30ef7f0400b16f2927570
Md. Sahin Alan <sahinraj20@gmail.com>
2025-11-12 20:03:38 +0600

    complete attendance functionality

--- backend/src/features/attendance/attendance.controller.ts
+++ backend/src/features/attendance/attendance.controller.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express";
-import { updateAttendance } from "./attendance.service";
+import { getAttendance, updateAttendance } from "./attendance.service";
+
+
+// Get attendances for a specific date
+export const getAttendanceController = async (req: Request, res: Response) => {
+  try {
+    const { date } = req.query;
+    if (!date) return res.status(400).json({ success: false, message: "Date is required" });
+
+    const attendances = await getAttendance(date as string);
+    res.status(200).json({ success: true, message:"Successfully fetched.", data: attendances });
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
 
 // Update attendance
 export const updateAttendanceController = async (req: Request, res: Response) => {
