011030dd6ebad53a0eaab22e185516cff343c2d1
Md. Sahin Alan <sahinraj20@gmail.com>
2025-11-23 18:54:26 +0600

    added get sales info function in sales store

--- frontend/components/salesPage/SalesBody.tsx
+++ frontend/components/salesPage/SalesBody.tsx
@@ -17,6 +17,7 @@ const SalesBody = () => {
       <div>
         <SalesPagination></SalesPagination>
       </div>
+      
     </div>
   )
 }
--- frontend/stores/salesStore.ts
+++ frontend/stores/salesStore.ts
@@ -10,18 +10,56 @@ interface SalePayload {
   buyerMobileNumber: string;
 }
 
+interface Sale {
+  _id: string;
+  productId: string;
+  productName: string;
+  unit: number;
+  totalPrice: number;
+  buyerName: string;
+  buyerMobileNumber: string;
+  createdAt: string;
+}
+
 interface SalesStore {
+  sales: Sale[];
+  total: number;
+  isLoadingSales: boolean;
   isAddingSale: boolean;
+
+  getAllSales: () => Promise<void>;
   addNewSale: (data: SalePayload) => Promise<void>;
 }
 
 export const useSalesStore = create<SalesStore>((set) => ({
+  sales: [],
+  total: 0,
+  isLoadingSales: false,
   isAddingSale: false,
 
+  // 🔹 FETCH ALL SALES
+  getAllSales: async () => {
+    set({ isLoadingSales: true });
+    try {
+      const response = await axiosInstance.get<{data:Sale[], total:number}>("/sales");
+
+      set({
+        sales: response.data.data || [],
+        total: response.data.total || 0,
+      });
+
+    } catch (err) {
+      console.error("Error fetching sales:", err);
+      throw err;
+    } finally {
+      set({ isLoadingSales: false });
+    }
+  },
+
+  //add new sale
   addNewSale: async (data: SalePayload) => {
     set({ isAddingSale: true });
     try {
-      // Ensure productName is defined
       if (!data.productName) throw new Error("Product name is required");
 
       await axiosInstance.post("/sales/add", data);
