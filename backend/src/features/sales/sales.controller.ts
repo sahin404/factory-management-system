93a7f8f92108aaf638791cbe8bd790508bc1ffd7
Md. Sahin Alan <sahinraj20@gmail.com>
2025-11-23 18:30:13 +0600

    added get sales controller

--- backend/src/features/sales/sales.controller.ts
+++ backend/src/features/sales/sales.controller.ts
@@ -1,13 +1,46 @@
 import { Request, Response } from "express";
-import { addSales } from "./sales.service";
+import { addSales, getAllSales } from "./sales.service";
 
-// add sales 
+// get all sales
+export const getAllSalesController = async (req: Request, res: Response) => {
+  try {
+    const response = await getAllSales();
+    res.status(200).json({
+      success: true,
+      message: "Successfully fetched the data!",
+      data: response,
+    });
+  } catch (err:any) {
+    console.error("Error fetching sales:", err);
+
+    return res.status(500).json({
+      success: false,
+      message: err.message || "Failed to fetch sales!",
+    });
+  }
+};
+
+// add sales
 export const addSalesController = async (req: Request, res: Response) => {
   try {
-    const { productId, unit, totalPrice, buyerName, buyerMobileNumber, productName } = req.body;
+    const {
+      productId,
+      unit,
+      totalPrice,
+      buyerName,
+      buyerMobileNumber,
+      productName,
+    } = req.body;
 
     // validation
-    if (!productId || !unit || !totalPrice || !buyerName || !buyerMobileNumber || !productName) {
+    if (
+      !productId ||
+      !unit ||
+      !totalPrice ||
+      !buyerName ||
+      !buyerMobileNumber ||
+      !productName
+    ) {
       return res.status(400).json({
         success: false,
         message: "All fields are required!",
