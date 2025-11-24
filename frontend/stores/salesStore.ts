ce0b267a5d3e24b56264a8d0414dd9561f571be6
Md. Sahin Alan <sahinraj20@gmail.com>
2025-11-24 13:26:22 +0600

    create optimistic UI change in sales deletion

--- frontend/stores/salesStore.ts
+++ frontend/stores/salesStore.ts
@@ -27,31 +27,32 @@ interface SalesStore {
   total: number;
   isLoadingSales: boolean;
   isAddingSale: boolean;
-  isDeleting:boolean;
+  isDeleting: boolean;
 
   getAllSales: () => Promise<void>;
   addNewSale: (data: SalePayload) => Promise<void>;
-  deleteSale:(id:string) => void;
+  deleteSale: (id: string) => void;
 }
 
-export const useSalesStore = create<SalesStore>((set) => ({
+export const useSalesStore = create<SalesStore>((set, get) => ({
   sales: [],
   total: 0,
   isLoadingSales: false,
   isAddingSale: false,
-  isDeleting:false,
+  isDeleting: false,
 
   // get all sales
   getAllSales: async () => {
     set({ isLoadingSales: true });
     try {
-      const response = await axiosInstance.get<{data:Sale[], total:number}>("/sales");
+      const response = await axiosInstance.get<{ data: Sale[]; total: number }>(
+        "/sales"
+      );
 
       set({
         sales: response.data.data || [],
         total: response.data.total || 0,
       });
-
     } catch (err) {
       console.error("Error fetching sales:", err);
       throw err;
@@ -67,7 +68,6 @@ export const useSalesStore = create<SalesStore>((set) => ({
       if (!data.productName) throw new Error("Product name is required");
 
       await axiosInstance.post("/sales/add", data);
-
     } catch (err) {
       console.log("Error adding sale:", err);
       throw err;
@@ -77,18 +77,21 @@ export const useSalesStore = create<SalesStore>((set) => ({
   },
 
   // delete a sale
-  deleteSale: async(saleId)=>{
-    set({isDeleting:true});
-    try{
+  deleteSale: async (saleId) => {
+    const { sales } = get();
+    const prevState = sales;
+    //optimistic delete
+    set({
+      sales: sales.filter((sale) => sale._id !== saleId),
+      isDeleting: true,
+    });
+
+    try {
       await axiosInstance.delete(`/sales/${saleId}`);
+    } catch (err) {
+      console.log("Something went wrong.");
+    } finally {
+      set({ isDeleting: false });
     }
-    catch(err){
-      console.log('Something went wrong.');
-    }
-    finally{
-      set({isDeleting:false});
-    }
-  }
-
-
+  },
 }));
