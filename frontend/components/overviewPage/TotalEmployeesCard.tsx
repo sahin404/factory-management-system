c4e51561bed9e2a139e5fd7673e088b885dcdebd
Md. Sahin Alan <sahinraj20@gmail.com>
2025-11-25 15:12:49 +0600

    added total employee function in store

--- frontend/components/overviewPage/TotalEmployeesCard.tsx
+++ frontend/components/overviewPage/TotalEmployeesCard.tsx
@@ -1,6 +1,19 @@
+"use client"
+
+import { useOverviewStore } from "@/stores/overviewStore"
+import { useEffect } from "react";
+
 const TotalEmployeesCard = () => {
+  const {totalEmployees, getTotalEmployees, gettingTotalEmployees} = useOverviewStore();
+
+  useEffect(()=>{
+    getTotalEmployees();
+  },[])
+
+  // todo: skeleton
+
   return (
-    <div className='bg-black'>TotalEmployeesCard</div>
+    <div className='bg-black text-white'>{totalEmployees}</div>
   )
 }
 
