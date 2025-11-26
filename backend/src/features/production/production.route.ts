c8c2a39e4398259f9f5d0fed0d1139063a3b89ce
Md. Sahin Alan <sahinraj20@gmail.com>
2025-11-26 15:46:33 +0600

    secure production routes

--- backend/src/features/production/production.route.ts
+++ backend/src/features/production/production.route.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import { addProductController, deleteProductByIdController, getProductByIdController, getProductController, updateAllFieldsController, updateProductQuantityController} from "./production.controller";
+import { verifyToken } from "../../middlewares/auth.middleware";
+import { verifyAdmin } from "../../middlewares/adminVerify.middlware";
+import { verifyManager } from "../../middlewares/managerVerify.middleware";
 
 const productRouter = express.Router();
 
-productRouter.get("/", getProductController);
-productRouter.get("/:id", getProductByIdController);
-productRouter.patch("/:id", updateProductQuantityController);
-productRouter.put("/:id", updateAllFieldsController);
-productRouter.delete("/:id", deleteProductByIdController);
-productRouter.post("/", addProductController);
+productRouter.get("/", verifyToken, verifyAdmin, verifyManager, getProductController);
+productRouter.get("/:id", verifyToken, verifyAdmin, verifyManager, getProductByIdController);
+productRouter.patch("/:id", verifyToken, verifyAdmin, verifyManager, updateProductQuantityController);
+productRouter.put("/:id", verifyToken, verifyAdmin, verifyManager, updateAllFieldsController);
+productRouter.delete("/:id", verifyToken, verifyAdmin, verifyManager, deleteProductByIdController);
+productRouter.post("/", verifyToken, verifyAdmin, verifyManager, addProductController);
 
 export default productRouter;
