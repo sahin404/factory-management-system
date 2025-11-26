918a82ac824df8394b8e7873183d7fc693113182
Md. Sahin Alan <sahinraj20@gmail.com>
2025-11-26 15:50:46 +0600

    secure attendances routes

--- backend/src/features/attendance/attendance.route.ts
+++ backend/src/features/attendance/attendance.route.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { getAttendanceController, updateAttendanceController } from "./attendance.controller";
+import { verifyManager } from "../../middlewares/managerVerify.middleware";
+import { verifyAdmin } from "../../middlewares/adminVerify.middlware";
+import { verifyToken } from "../../middlewares/auth.middleware";
 
 const attendanceRouter = Router();
 
-attendanceRouter.get("/", getAttendanceController);
-attendanceRouter.post("/update", updateAttendanceController);
+attendanceRouter.get("/", verifyToken, verifyAdmin, verifyManager, getAttendanceController);
+attendanceRouter.post("/update",verifyToken, verifyAdmin, verifyManager, updateAttendanceController);
 
 export default attendanceRouter;
