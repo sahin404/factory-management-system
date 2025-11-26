803f8370226a9ad0f672ba50330b6a778ec2a791
Md. Sahin Alan <sahinraj20@gmail.com>
2025-11-26 15:47:29 +0600

    secure salary routes

--- backend/src/features/salary/salary.route.ts
+++ backend/src/features/salary/salary.route.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { addSalaryController, getSalaryController } from "./salary.controller";
+import { verifyToken } from "../../middlewares/auth.middleware";
+import { verifyAdmin } from "../../middlewares/adminVerify.middlware";
+import { verifyAccountant } from "../../middlewares/accountantVerify.middleware";
 
 const salaryRouter = express.Router();
 
-salaryRouter.post("/", addSalaryController);
-salaryRouter.get("/", getSalaryController);
+salaryRouter.post("/", verifyToken, verifyAdmin, verifyAccountant, addSalaryController);
+salaryRouter.get("/",  verifyToken, verifyAdmin, verifyAccountant, getSalaryController);
 
 
 export default salaryRouter;
