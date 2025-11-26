b1f191461a8a1f8c4579b0449c17efc8a4404631
Md. Sahin Alan <sahinraj20@gmail.com>
2025-11-26 15:49:10 +0600

    secure sales routes

--- backend/src/features/sales/sales.routes.ts
+++ backend/src/features/sales/sales.routes.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import { addSalesController, deleteSalesController, getAllSalesController } from './sales.controller';
+import { verifyManager } from '../../middlewares/managerVerify.middleware';
+import { verifyAdmin } from '../../middlewares/adminVerify.middlware';
+import { verifyToken } from '../../middlewares/auth.middleware';
 
 const salesRouter = express.Router();
 
 
-salesRouter.get('/', getAllSalesController);
-salesRouter.post('/add', addSalesController);
-salesRouter.delete('/:id', deleteSalesController);
+salesRouter.get('/',  verifyToken, verifyAdmin, verifyManager, getAllSalesController);
+salesRouter.post('/add', verifyToken, verifyAdmin, verifyManager, addSalesController);
+salesRouter.delete('/:id', verifyToken, verifyAdmin, verifyManager, deleteSalesController);
 
 
 export default salesRouter;
